dd401f4b7b9541de765bf048e0177447048a441e
T-unity <takadanaritoshi@gmail.com>
2020-10-27 12:20:46 +0900

    ユーザーの削除機能を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,21 +21,53 @@ class UsersController < ApplicationController
     @email = res_body["data"]["email"]
   end
 
+  def new
+  end
+
+  def create
+  end
+
   def edit
     res = Faraday.get "ホスト名/api/v1/users/#{params[:id]}"
-    @user = JSON.parse(res.body)
+    res_body = JSON.parse(res.body)
+
+    @name = res_body["data"]["name"]
+    @email = res_body["data"]["email"]
   end
 
   def update
-    # req = Faraday.put "ホスト名/api/v1/users/#{params[:id]}"
-    # body = {
-    #   "user": {
-    #     "name": "hoge"
-    #     "email" : 
-    #   }
-    # }.to_json
+    req = Faraday.put "ホスト名/api/v1/users/#{params[:id]}"
+    data = {
+      "user": {
+        "name" => "#{params[:name]}",
+        "email" => "#{params[:email]}",
+      }
+    }.to_json
 
-    # req.body = body
+    req.body = data
   end
+
+  def destroy
+    Faraday.delete "ホスト名/api/v1/users/#{params[:id]}"
+    redirect_to users_path
+  end
+
+  private
+      def user_params
+        params.require(:user).permit(
+            :name,
+            :email, 
+          )
+      end
+
+
+  # def update_customer(user_id, customer_id, body)
+  #   res = @connection.put do |req|
+  #     req.url("/admin/api/2020-07/customers/#{customer_id}.json")
+  #     req.headers["Content-Type"] = "application/json"
+  #     req.body = body
+  #   end
+  #   check_response_status(__method__, user_id, customer_id, res)
+  # end
 
 end
